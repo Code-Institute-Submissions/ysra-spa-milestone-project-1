b567d9148bfcd23bbd3d8528b6ba0922308d06f6
Saharalnoor <baiarrah_04@yahoo.com>
2020-04-16 21:36:15 +0000

    changing codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,4 @@
-window.onscroll = function () {
-    scrollFunction();
-};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        $(".navbar").addClass('invisible');
-    } else {
-        $(".navbar").removeClass('invisible');
-    }
-}
 
 $('.navbar-toggler-icon').click(function() {
     $('.navbar').toggleClass('bg-white');
